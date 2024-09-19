76abba5a020cff483174710d3d3b310a56c52513
Lootii Kiri <Lootii15@gmail.com>
2024-09-19 01:19:04 -0400

    changed hobbies section to a gallery like display

---
+++ hobbies.js
@@ -0,0 +1,53 @@
+const hobbies = [
+    {
+        title: 'Basketball',
+        media: '<img src="images/coding2.png" alt="coding2" class="hobby-media">'
+    },
+    {
+        title: 'Fitness',
+        media: '<img src="images/coding2.png" alt="Fitness" class="hobby-media">'
+    },
+    {
+        title: 'Learning (reading books, watching podcasts)',
+        media: '<img src="images/coding2.png" alt="Learning" class="hobby-media">'
+    },
+    {
+        title: 'Coding',
+        media: '<img src="images/coding2.png" alt="coding2" class="hobby-media"><img src="images/coding.JPG" alt="Coding" class="hobby-media"><img src="images/coding3.png" alt="coding3" class="hobby-media">'
+    },
+    {
+        title: 'Exploring new technologies',
+        media: '<img src="images/coding2.png" alt="Exploring new technologies" class="hobby-media">'
+    }
+];
+
+let currentHobbyIndex = 0;
+
+function showHobby(index) {
+    const hobbyDisplay = document.getElementById('hobby-display');
+    hobbyDisplay.innerHTML = `
+    <div class="hobby-item">
+        <div class="hobby-title">${hobbies[index].title}</div>
+        <div class="hobby-images">
+            ${hobbies[index].media}
+        </div>
+    </div>`;
+}
+
+function showNextHobby() {
+    console.log("Next hobby function called");
+    currentHobbyIndex = (currentHobbyIndex + 1) % hobbies.length;
+    showHobby(currentHobbyIndex);
+}
+
+function showPreviousHobby() {
+    console.log("Previous hobby function called");
+    currentHobbyIndex = (currentHobbyIndex - 1 + hobbies.length) % hobbies.length;
+    showHobby(currentHobbyIndex);
+}
+
+// Initialize by showing the first hobby
+showHobby(currentHobbyIndex);
+
+
+
